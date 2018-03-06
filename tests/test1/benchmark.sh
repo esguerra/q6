ffdd5a5dc6f64474be3bf08c594241619c627caf
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2018-03-06 09:57:40 +0100

    minor changes for csb test. change to new module use at csb.

--- tests/test1/benchmark.sh
+++ tests/test1/benchmark.sh
@@ -2,8 +2,10 @@
 #set -x
 #trap read debug
 
-export rcran=/home/apps/R
-export PATH=$rcran/bin:$PATH
+#export rcran=/home/apps/R
+#export PATH=$rcran/bin:$PATH
+
+module load r/3.4.1
 
 echo "The following is the Q heating time in minutes: "
     grep "Total time of main loop:" eq*.log | awk '{print $6}' | sed 's/(s)//g' \
