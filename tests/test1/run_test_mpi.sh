9a7cd522c3c7a70d44735cb40771b6bf1179f966
Mauricio Esguerra <mauricio.esguerra@gmail.com>
2019-01-13 19:55:28 +0100

    docker allows only two cpu's
    docker: Error response from daemon: Requested CPUs are not available - requested 0-3, available: 0-1.

--- tests/test1/run_test_mpi.sh
+++ tests/test1/run_test_mpi.sh
@@ -30,7 +30,7 @@ fi
 # CORES=`grep processor /proc/cpuinfo | wc -l`
 NUMCORES=`grep processor /proc/cpuinfo | wc -l`
 # CORES=`grep "cpu cores" /proc/cpuinfo | awk '{print $4}' | paste -sd+ | bc`
-CORES=4
+CORES=2
 echo "Running simulation on $CORES cores out of $NUMCORES available"
 
 rm -f eq{1..5}.log dc{1..5}.log >& /dev/null
